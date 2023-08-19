4e7e239a6bb8d64e148f531a44b96eeb04d49534
Andrey Bigalinov <greengiovany@yandex.ru>
2023-08-19 19:27:12 +0700

    Сделана верстка, добавлены вью модели и моки

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,12 +1,70 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 }
 
+
+
+// MARK: ViewModels
+// для состояния "Вопрос показан"
+struct QuizStepModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+// для состояния "Результат квиза"
+struct QuizREsultViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+// MARK: Mocks
+private let questions: [QuizQuestion] = [
+    QuizQuestion(image: "The Godfather",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Dark Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Kill Bill",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Avengers",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Deadpool",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "The Green Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: true),
+    QuizQuestion(image: "Old",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Tesla",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false),
+    QuizQuestion(image: "Vivarium",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 correctAnswer: false)
+]
 /*
  Mock-данные
  
